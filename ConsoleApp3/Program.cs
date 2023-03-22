9e2a4df01659221ea371e0eef01c3e3325151cd0
nikitos <nikitos@DESKTOP-TL884D3>
2023-03-22 13:30:59 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            int min = array.Min();
+
+            int Average = (int)array.Average();
+
+            int max = array.Max();
+          
+            int sum = array.Sum();
+
+            
+            Console.WriteLine(min);
+
+            Console.WriteLine(Average);
+
+            Console.WriteLine(max);
+            
+            Console.WriteLine(sum);
+
+         
+
+            
+
+
+            
+        }
+    }
+}
